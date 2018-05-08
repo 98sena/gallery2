f71a3214089c142e348a8fa8842a61052d11cadc
98sena <37644146+98sena@users.noreply.github.com>
2018-05-09 00:15:42 +0900

    Add files via upload

---
+++ gallery2_180508_1.js
@@ -0,0 +1,45 @@
+var img;
+var xPos, yPos;
+function preload() {
+  img = loadImage('img1.jpg');
+}
+function setup() {
+  createCanvas(640,800);
+  background(0);
+  noStroke()
+  img.loadPixels();
+  xPos=width/2;
+  yPos=height/2;
+}
+
+function draw() {
+  
+  if(xPos<0){
+    xPos=width-20;
+  }
+  else if(xPos>width){
+    xPos=20;
+  }
+  else if(yPos<0){
+    yPos=height-10;
+  }
+  else if(yPos>height){
+    yPos=10;
+  }
+  var c= img.get(xPos,yPos);
+  fill(c,100);
+  stroke(c);
+  var diam= 20.0;
+  strokeWeight(5);
+  rectMode(CENTER);
+  rect(xPos,yPos,diam,diam);
+
+}
+function keyPressed() {
+  
+  if(keyCode ===LEFT_ARROW) xPos-=30;
+  else if(keyCode ===RIGHT_ARROW) xPos+=30;
+  else if(keyCode ===UP_ARROW) yPos-=30;
+  else if(keyCode ===DOWN_ARROW) yPos+=30;
+ 
+}
